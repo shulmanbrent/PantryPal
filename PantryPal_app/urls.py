98cb19c4213ab4f3b3478ca9535f3d04a64d831c
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-27 17:40:29 -0500

    Trying to fix issue with static media on Heroku

--- PantryPal_app/urls.py
+++ PantryPal_app/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
 from PantryPal_app import views
+from PantryPal import settings
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_URL}),
+
+)
