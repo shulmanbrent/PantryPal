3237c0fe164656fb57eb2fc707869abdd59a3187
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-27 19:45:41 -0500

    more attempts to fix static

--- PantryPal/urls.py
+++ PantryPal/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += patterns('',
-    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_PATH}),
 )
