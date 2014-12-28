868423fb07849a890b6b8fc43722eef033781bc6
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-27 21:42:38 -0500

    All static issues fixed!

--- PantryPal/wsgi.py
+++ PantryPal/wsgi.py
@@ -13,7 +13,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PantryPal.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
