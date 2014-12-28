3c6c560506d8a00cd79ffef0b0648e606a26a401
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-27 20:49:25 -0500

    Cleaned urls.py for PantryPal

--- PantryPal/urls.py
+++ PantryPal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from PantryPal import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'PantryPal.views.home', name='home'),
@@ -8,8 +9,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^PantryPal_app/', include('PantryPal_app.urls')),
-)
-
-urlpatterns += patterns('',
     (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
