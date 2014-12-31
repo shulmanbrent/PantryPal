4a6ecb0eebb1290ac48c8d3318b6f6d7870c47ca
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-30 22:55:04 -0500

    Backed up!

---
+++ PantryPal_app/run_query.py
@@ -0,0 +1,71 @@
+import json
+import urllib, urllib2
+
+def run_query(search_terms):
+    # Specify the root
+    root_url = 'http://api.yummly.com/v1/api/recipes?'
+
+    # Specify how many results we wish to be returned per page.
+    # Offset specifies where in the results list to start from.
+    # With maxResults = 10 and start = 11, this would start from page 2.
+    maxResult = 12
+    start = 0
+
+    # Setup authentication with the Yummly servers.
+    yummly_app_id = '92d67e12'
+    yummly_api_key = '1c9dd40cdaa1ee28b9a65429530fcfe6'
+    
+
+    search_terms = search_terms.split()
+
+    
+    # Construct the latter part of our request's URL.
+    # Sets the format of the response to JSON and sets other properties.
+    search_url = "{0}_app_id={1}&_app_key={2}&maxResult={3}&start={4}".format(
+        root_url,
+        yummly_app_id,
+        yummly_api_key,
+        maxResult,
+        start)
+    
+    # for term in query:
+    #     ingredient = "&allowedIngredient[]={0}".format(
+    #         term)
+    #     search_url += ingredient
+
+
+    for term in search_terms:
+        ingredient = "&allowedIngredient[]={0}".format(
+            term)
+        search_url += ingredient    
+
+
+    # Create a 'password manager' which handles authentication for us.
+    #password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    #password_mgr.add_password(None, search_url, username, bing_api_key)
+
+    # Create our results list which we'll populate.
+    results = []
+
+    try:
+        # Connect to the server and read the response generated.
+        response = urllib2.urlopen(search_url).read()
+
+        # Convert the string response to a Python dictionary object.
+        json_response = json.loads(response)
+        for recipe in json_response['matches']:
+            results.append(recipe)
+
+        # Loop through each page returned, populating out results list.
+        #for result in json_response['d']['results']:
+        #    results.append({
+        #        'title': result['Title'],
+        #        'link': result['Url'],
+        #        'summary': result['Description']})
+
+    # Catch a URLError exception - something went wrong when connecting!
+    except urllib2.URLError, e:
+        print "Error when querying the Yummly API: ", e
+
+    # Return the list of results to the calling function.
+    return results
--- PantryPal_app/views.py
+++ PantryPal_app/views.py
@@ -1,7 +1,10 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from PantryPal_app.run_query import run_query
+
 
 def index(request):
     # Request the context of the request.
@@ -10,9 +13,28 @@ def index(request):
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Yummly function to get the results list!
+            result_list = run_query(query)
+
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response('PantryPal_app/index.html', context_dict, context)
+    return render_to_response('PantryPal_app/index.html', {'result_list': result_list}, context)
+
+# def search(request):
+#     context = RequestContext(request)
+
+
+#     return render_to_response('PantryPal_app/', {'result_list': result_list}, context)
+
+# def recipes(request):
+#     context = RequestContext(request)
+
+#     return render_to_response('PantryPal_app/recipes.html', context)
