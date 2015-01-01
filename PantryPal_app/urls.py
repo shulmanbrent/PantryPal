3a9e4348be7c64b0235d52494746bc69019b00a6
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-31 19:23:18 -0500

    added url patterns for search

--- PantryPal_app/urls.py
+++ PantryPal_app/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 from PantryPal_app import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
-
+        url(r'^$', views.index, name='index'),
+        url(r'^search/', views.search, name='search'),
 )
