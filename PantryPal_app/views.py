a2ec8acfa93c5447f55ba1938193d85bc7ee2e2a
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-31 19:23:38 -0500

    included search view

--- PantryPal_app/views.py
+++ PantryPal_app/views.py
@@ -28,11 +28,25 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
     return render_to_response('PantryPal_app/index.html', {'result_list': result_list}, context)
 
-# def search(request):
-#     context = RequestContext(request)
+def search(request):
+    context = RequestContext(request)
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Yummly function to get the results list!
+            result_list = run_query(query)
 
 
-#     return render_to_response('PantryPal_app/', {'result_list': result_list}, context)
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+    return render_to_response('PantryPal_app/search.html', {'result_list': result_list}, context)
 
 # def recipes(request):
 #     context = RequestContext(request)
