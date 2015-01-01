ce634de58701a4b85edf35dcd89471b9447ffdf5
shulmanbrent <shulmanbrent@yahoo.com>
2014-12-31 20:12:59 -0500

    fixed weird non loading bug

--- static/js/PantryPal-ajax.js
+++ static/js/PantryPal-ajax.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 	$("#user_form").submit(function(event) {
 		$("#search_content").hide();
 	    $.post('/PantryPal_app/search/', $(this).serialize() , function(data){
 	    	$("#search_content").html(data);
 	    });
+	    $("#search_content").show();
 	    $("#search_content").fadeIn();
 	    event.preventDefault();
 	});
-});
+// });
 
 
