2a64c7167fb5c49c18196f1f511019ed94ba5acc
shulmanbrent <shulmanbrent@yahoo.com>
2015-01-01 23:29:09 -0500

    added ajax monitor to display spinner

--- static/js/PantryPal-ajax.js
+++ static/js/PantryPal-ajax.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 	$("#user_form").submit(function(event) {
-		// $("#search_content").hide();
+		// $("#spinner").css("display", "inline-block");
 	    $.post('/PantryPal_app/search/', $(this).serialize() , function(data){
 	    	$("#search_content").html(data);
 	    });
 	    $("#search_content").show();
 	    $("#search_content").fadeIn();
+	    // $('#spinner').css('display', 'none');
 	    event.preventDefault();
 	});
 });
 
+$(document).ajaxStart(function() {
+	$('#spinner').css("display", "inline-block");
+	$('#container-folio').css("display", "none");
+}).ajaxStop(function() {
+	$('#spinner').css("display", "none");
 
+});
