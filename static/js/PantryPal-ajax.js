1d92c1b3c67ff147f0df7c0a44f283b95eda2442
shulmanbrent <shulmanbrent@yahoo.com>
2015-01-02 02:13:04 -0500

    added fade in feature for recipes

--- static/js/PantryPal-ajax.js
+++ static/js/PantryPal-ajax.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 	$("#user_form").submit(function(event) {
-		// $("#spinner").css("display", "inline-block");
 	    $.post('/PantryPal_app/search/', $(this).serialize() , function(data){
-	    	$("#search_content").html(data);
+	    	$("#search_content").hide().html(data).fadeIn(1500);
 	    });
 	    $("#search_content").show();
 	    $("#search_content").fadeIn();
-	    // $('#spinner').css('display', 'none');
 	    event.preventDefault();
 	});
 });
 
 $(document).ajaxStart(function() {
+	$("#search_content").html("");
 	$('#spinner').css("display", "inline-block");
 	$('#container-folio').css("display", "none");
 }).ajaxStop(function() {
 	$('#spinner').css("display", "none");
+	$('#search_content').css("display", "inherit");
 
 });
