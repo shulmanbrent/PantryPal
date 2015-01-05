25962a6a50a82e5c244fa77b16430c73f4fea675
shulmanbrent <shulmanbrent@yahoo.com>
2015-01-04 20:10:20 -0500

    included initial index redirect

--- PantryPal/urls.py
+++ PantryPal/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from PantryPal import settings
+from django.http import HttpResponseRedirect
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^PantryPal_app/', include('PantryPal_app.urls')),
     url(r'^static/', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^', lambda r: HttpResponseRedirect('PantryPal_app/'))
 )
