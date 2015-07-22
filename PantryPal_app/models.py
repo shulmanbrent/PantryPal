f9d3733252dae11012856ce635710ef68c8d7b2d
shulmanbrent <shulmanbrent@yahoo.com>
2015-07-21 23:35:45 -0700

    Created Query model to monitor users queres

--- PantryPal_app/models.py
+++ PantryPal_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Query(models.Model):
+	id = models.IntegerField(primary_key=True)
+	user =  models.CharField(max_length=30)
+	query = models.CharField(max_length=200)
+
+	def __unicode__():
+		return self.user + ": " + query
