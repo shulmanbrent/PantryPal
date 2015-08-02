e994e111ec6c0e3e945c69bf0776b84717b538bb
shulmanbrent <shulmanbrent@yahoo.com>
2015-08-02 10:20:08 -0700

    Made slight change to PantryPal_app url

--- PantryPal_app/urls.py
+++ PantryPal_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^search/$', views.search, name='search'),
         url(r'^register/$', views.register, name='register'),
-        url(r'^login/$', views.user_login, name='login'),
+        url(r'^login/$', views.user_login, name='user_login'),
         url(r'^logout/$', views.logout_view, name='logout_view'),
 )
